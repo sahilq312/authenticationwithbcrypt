becbfe72bf0bbb02594b9ee5573ecbaae97e182c
sahil <120485501+sahilq312@users.noreply.github.com>
2023-02-28 00:15:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express")
+const bodyParser = require("body-parser")
+const ejs = require("ejs")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const saltRounds = 10;
+
+
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+
+
+console.log(process.env.API_KEY);
+
+const userSchema = new mongoose.Schema({
+    email: "String",
+    password: "String"
+})
+
+
+const User =  new mongoose.model("User", userSchema)
+
+const app = express();
+
+
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.render("home")
+})
+
+app.get("/login", function(req, res){
+    res.render("login")
+})
+
+app.get("/register", function(req, res){
+    res.render("register")
+})
+
+app.post("/register", function(req,res){
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else {
+                res.render("secrets")
+            }
+            
+        })
+    })
+    
+    });
+    
+app.post("/login", function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err,  foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            if(foundUser){
+                // Load hash from your password DB.
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                 // result == true
+                 if(result === true){
+                    res.render("secrets")
+                 }
+                });
+            }
+        }
+    })
+})
+
+
+
+
+app.listen(3000, function(){
+    console.log("server is running on port 3000");
+})
